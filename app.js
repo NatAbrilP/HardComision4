340f1074c5377f8929e83ae99e335b8cebf35f11
Lucas Cardozo <ishida393@gmail.com>
2020-11-18 17:15:19 -0300

    modifique app.js

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
     // Route System
     // ¿?
+    res.end()
 }).listen(3030, 'localhost', () => console.log('Server running in 3030 port'));
