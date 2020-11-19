5a78147c8eade9819bcc6a64bd8bec1e7f7e5066
Lucas Cardozo <ishida393@gmail.com>
2020-11-19 13:57:51 -0300

    modifique home

--- src/homePage.js
+++ src/homePage.js
@@ -7,5 +7,18 @@ module.exports = {
     },
     cantidad: function(){
         return this.leerJSON().total_movies
+    },
+    titulos : function(){
+        return this.leerJSON().title
+    },
+    pelisOrden : function(){
+        let orden = []
+        this.leerJSON().movies.forEach((peli) => {
+            orden.push(peli.title);
+        })
+        return orden.sort(function(a,b){
+            return a.toLowerCase().loceleCompare(b.toLowerCase());
+        });
     }
-}
+    
+	}
--- src/index.js
+++ src/index.js
@@ -10,13 +10,17 @@ const { totalMovies } = require("./homePage");
 module.exports = {
    homePage: function (req, res) {
        res.write("​Bienvenidos a DH Movies el mejor sitio para encontrar las mejores películas, incluso mucho mejor que Netflix, Cuevana y PopCorn​.\n\n")
-       res.write("El total de peliculas en cartelera en estos momentos :\n" )
-       res.write("Nuestro catalogo de peliculas incluye :" + homePage.cantidad() + "\n\n")
+       res.write("El total de peliculas en cartelera en estos momentos : " + homePage.cantidad() + "\n")
+       res.write("Nuestro catalogo de peliculas incluye : " + "\n\n")
+       homePage.leerJSON().movies.forEach(pelicula => {
+        res.write(pelicula.title.toUpperCase() + "\n\n")
+    });
        res.write("\n\n\n")
        res.write("​Recordá que podés visitar las secciones:\n\n* En Cartelera\n* Más Votadas\n* Sucursales\n* Contacto\n* Preguntas Frecuentes\n")
-       homePage.leerJSON().movies.forEach(pelicula => {
-           res.write(pelicula.title)
-       });
+       /* homePage.leerJSON().movies.forEach(pelicula => {
+        res.write(pelicula.title)
+    }); */
+    /*    res.write(homePage.pelisOrden()) */
        res.end();
 
 
