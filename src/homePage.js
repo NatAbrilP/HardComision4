0fd297013c137ac63b0cde0db5b13d455e5960b2
Lucas Cardozo <ishida393@gmail.com>
2020-11-19 12:18:47 -0300

    modofique home

--- src/homePage.js
+++ src/homePage.js
@@ -1,12 +1,5 @@
-const fs=require("fs")
-module.exports={
-    bd: "./data/movies.json",
-    leerJSON :function(){
-      /* let moviesJSON = fs.readFileSync(this.bd,"utf-8")
-      let movies = JSON.parse(moviesJSON) */
-    return movies = JSON.parse( fs.readFileSync(this.bd,"utf-8"))
-    },
-     totalMovies:function(){
-      return this.leerJSON().total_movies
-    }, 
-}
+const fs = require('fs');
+
+let moviesParse = JSON.parse(fs.readFileSync('./data/movies.json', 'utf-8'))
+
+let totalM
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-let homePage = require("./homePage");
+let homePage = require("./");
 let enCartelera = require("./enCartelera")
 
 
@@ -11,10 +11,10 @@ module.exports = {
    homePage: function (req, res) {
        res.write("​Bienvenidos a DH Movies el mejor sitio para encontrar las mejores películas, incluso mucho mejor que Netflix, Cuevana y PopCorn​.\n\n")
        res.write("El total de peliculas en cartelera en estos momentos :\n" )
-       res.write("Nuestro catelogo de peliculas incluye : \n\n")
+       res.write("Nuestro catalogo de peliculas incluye : \n\n")
        res.write("\n\n\n")
-       res.write("​Recordá que podés visitar las secciones:\n\n* En Cartelera\n* Más Votadas\n* Sucursales\n* Contacto\n* Preguntas Frecuentes")
-     
+       res.write("​Recordá que podés visitar las secciones:\n\n* En Cartelera\n* Más Votadas\n* Sucursales\n* Contacto\n* Preguntas Frecuentes\n")
+       res.write(res.write(`- ${titulo}\n`))
        res.end();
 
 
